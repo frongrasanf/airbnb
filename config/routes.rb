f352f7729e79bc157c96ba7e7e77a14240512a62
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-14 12:57:42 +0900

    modify typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'rooms#index'
   resources :rooms do
-    resources :bookings, only: %i[new, create]
+    resources :bookings, only: %i[new create]
   end
 end
