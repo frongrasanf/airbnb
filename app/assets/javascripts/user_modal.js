133ba1bc0df0c2680f6af9fb2cd5f8b434b8e00e
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-18 17:18:58 +0900

    add sign_in modal element

--- app/assets/javascripts/user_modal.js
+++ app/assets/javascripts/user_modal.js
@@ -6,14 +6,31 @@ $(function(){
     $("body").append('<div id="modal-overlay"</div>');
     var w = $(window).width();
     var h = $(window).height();
-    var cw = $(".modal-content").outerWidth();
-    var ch = $(".modal-content").outerHeight();
+    var cw = $("#modal__new").outerWidth();
+    var ch = $("#modal__new").outerHeight();
     var pxleft = ((w - cw) / 2);
     var pxtop = ((h - ch) / 2);
-    $(".modal-content").css({ "left": pxleft + "px" });
-    $(".modal-content").css({ "top": pxtop + "px" });
+    $("#modal__new").css({ "left": pxleft + "px" });
+    $("#modal__new").css({ "top": pxtop + "px" });
     $("#modal-overlay").fadeIn("slow");
-    $(".modal-content").fadeIn("slow");
+    $("#modal__new").fadeIn("slow");
+  });
+
+  $(".modal-open_login").on("click", function(e) {
+    e.preventDefault();
+    $(this).blur();
+    if($("#modal-overlay")[0]) return false ;
+    $("body").append('<div id="modal-overlay"</div>');
+    var w = $(window).width();
+    var h = $(window).height();
+    var cw = $("#modal__login").outerWidth();
+    var ch = $("#modal__login").outerHeight();
+    var pxleft = ((w - cw) / 2);
+    var pxtop = ((h - ch) / 2);
+    $("#modal__login").css({ "left": pxleft + "px" });
+    $("#modal__login").css({ "top": pxtop + "px" });
+    $("#modal-overlay").fadeIn("slow");
+    $("#modal__login").fadeIn("slow");
   });
 
   $(".modal-close").on("click", function() {
