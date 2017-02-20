513066490e7bc33432d58914b17ea3a88fe1c41f
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-20 17:56:46 +0900

    add room create action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,8 +9,19 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    room = Room.new(room_params)
+    room.save
+  end
+
   def show
     @room = Room.find(params[:id])
     @user = User.new
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title, :prefecture, :room_type)
   end
 end
