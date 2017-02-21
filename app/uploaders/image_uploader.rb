c52163eb599ee7f5f49e08547808325025b59699
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-21 20:14:44 +0900

    delete image trimming size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,7 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  process resize_to_fill: [1280, 400]
+  # process resize_to_fill: [1280, 400]
 
   # Create different versions of your uploaded files:
   # version :thumb do
