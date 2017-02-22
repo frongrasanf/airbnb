5ca2008d66020935c325ab256eac29f90c41e6f3
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-22 21:27:32 +0900

    add booking create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+  def create
+    booking = Booking.new(booking_params)
+    if booking.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:check_in_date, :check_out_date).merge(user_id: current_user.id, room_id: params[:id])
+  end
 end
