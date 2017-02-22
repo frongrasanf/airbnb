12c5caa5ef0be6433a33846d3c81461b0c4272b8
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-22 20:26:28 +0900

    create booking table

--- db/migrate/20170222112156_create_bookings.rb
+++ db/migrate/20170222112156_create_bookings.rb
@@ -1,7 +1,10 @@
 class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
-
+      t.references :user, foreign_key: true, null: false
+      t.references :room, foreign_key: true, null: false
+      t.date :check_in_date, null: false
+      t.date :check_out_date, null: false
       t.timestamps
     end
   end
