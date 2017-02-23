22f5babab6703e85407089510c52f57d1533981b
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-23 20:13:12 +0900

    add search action for room controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'rooms#index'
   resources :rooms do
     resources :bookings, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
