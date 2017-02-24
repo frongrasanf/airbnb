9da94d2d8a10dfdce5d4d6b96f722990f8961240
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-24 14:11:18 +0900

    modify room controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,10 @@
 class RoomsController < ApplicationController
+
+  before_action :set_new_user, except: %i[new create]
+
   def index
     @room = Room.new
     @rooms = Room.order('id DESC').limit(3)
-    @user = User.new
   end
 
   def new
@@ -20,16 +22,18 @@ class RoomsController < ApplicationController
 
   def show
     @room = Room.find(params[:id])
-    @user = User.new
     @booking = Booking.new
   end
 
   def search
-    @user = User.new
     @rooms = Room.order('id DESC').limit(5)
   end
 
   private
+
+  def set_new_user
+    @user = User.new
+  end
 
   def room_params
     params.require(:room).permit(:title, :prefecture, :room_type, :image).merge(user_id: current_user.id)
