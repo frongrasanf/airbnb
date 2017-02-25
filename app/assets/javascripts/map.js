31ad1affad95b698b4acd98c5bd8105a6df63070
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-25 19:09:08 +0900

    modify location search

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,6 @@
 $(function(){
 
-  var hidden_keyword = $(".search__result__container__index");
-
+  // 地名入力アシスト
   function initialize() {
     var input = document.getElementById("location__input");
     var options = {
@@ -10,6 +9,10 @@ $(function(){
     autocomplete = new google.maps.places.Autocomplete( input, options);
   }
   google.maps.event.addDomListener( window, 'load', initialize);
+
+
+  // 検索結果のビューに地図を表示
+  var hidden_keyword = $(".search__result__container__index");
 
   if (hidden_keyword.length) {
     $(window).load(function() {
@@ -31,13 +34,32 @@ $(function(){
     });
   }
 
+  //ユーザーが入力した地名の緯度・経度を算出し、hiddenのvalueに返す
+  $(".search__form__submit__btn").mouseover(function(){
+    var input = document.getElementById("location__input");
+    var searchLocation = input.value;
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': searchLocation }, function(results, status){
+      if(status == google.maps.GeocoderStatus.OK) {
+        var lat = results[0].geometry.location.lat();
+        var lng = results[0].geometry.location.lng();
+        var lat1 = document.getElementById("hidden__latitude");
+        var lng1 = document.getElementById("hidden__longitude");
+        lat1.value = lat.toFixed(6);
+        lng1.value = lng.toFixed(6);
+      }
+    });
+  });
+
+
+  // 詳細画面に地図を表示
   if ($(".room__location").length) {
     var lat = $(".main__room__navi__location__lat").attr('data-lat');
     var lng = $(".main__room__navi__location__lng").attr('data-lng');
 
     var centerPosition = new google.maps.LatLng(lat, lng);
     var option = {
-      zoom : 18,
+      zoom : 15,
       center : centerPosition,
       mapTypeId : google.maps.MapTypeId.ROADMAP
     }
