ff1514e4c7d7512cf63b9d1825021a1d889b8d85
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-02-28 12:03:41 +0900

    add routing for review

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rooms#index'
   resources :rooms do
-    resources :bookings, only: :create
+    resources :bookings, only: :create do
+      resources :reviews, only: %i[new create]
+    end
     collection do
       get 'search'
     end
