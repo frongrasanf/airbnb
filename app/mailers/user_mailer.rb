25f7d3d5abf1441a35a3c33abc714fd2bc549d18
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-04 15:42:42 +0900

    modify mail content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,11 @@ class UserMailer < ApplicationMailer
   def registration
     @greeting = "Hi"
 
-    mail to: ENV['SAMPLE_ADDRESS']
+    mail(
+      to: ENV['SAMPLE_ADDRESS'],
+      subject: "会員登録完了のお知らせ"
+    ) do |format|
+      format.html
+    end
   end
 end
