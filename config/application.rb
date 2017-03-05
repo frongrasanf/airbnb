e019afb58453648c2e647bf8139cc5cd3359c7d3
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-05 14:01:47 +0900

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Airbnb
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
