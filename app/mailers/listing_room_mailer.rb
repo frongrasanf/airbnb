1b3a94cdc86825f770ceb30c26a78dea7bf5e7c5
frongrasanf <fron_gra_sanf@yahoo.co.jp>
2017-03-16 12:15:49 +0900

    modify listing room mailer

--- app/mailers/listing_room_mailer.rb
+++ app/mailers/listing_room_mailer.rb
@@ -1,2 +1,12 @@
 class ListingRoomMailer < ApplicationMailer
+  def listing(user)
+    @name = user.name
+
+    mail(
+      to: user.email,
+      subject: "リスティング完了のお知らせ"
+    ) do |format|
+      format.html
+    end
+  end
 end
